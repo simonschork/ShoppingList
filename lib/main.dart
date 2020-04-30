717e18e9f3492e09ace57ef73d0d070036ca1683
Simon Schork <simon.schork@gmail.com>
2020-04-30 20:03:53 +0200

    changed to seperated list, added textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:uuid/uuid.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'Einkaufsliste';
-  // Main app class
   @override
   Widget build(BuildContext context) {
-    // Build method is called on start
+    List<String> items = [
+      "Pasta",
+      "Kräuter",
+      "Zwiebeln",
+      "Essig",
+      "Spices",
+    ];
     return MaterialApp(
-        // Returns app with material design
-        title: _title,
-        theme: ThemeData(
-          // Set theme for app
-          primarySwatch: // Swatch for color palette for material design
-              Colors.blueGrey,
-          textTheme: GoogleFonts.robotoTextTheme(
-            Theme.of(context).textTheme,
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Einkaufsliste"),
         ),
-        home: Scaffold(
-            // App Content
-            appBar: AppBar(
-              // Title bar
-              centerTitle: true, // Center title text
-              title: const Text(_title), // Title text
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: new ListView.separated(
+                padding: const EdgeInsets.all(16.0),
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                    trailing: Icon(Icons.check),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    Divider(height: 8.0, color: Colors.blueGrey),
+              ),
             ),
-            body: Center(
-              child: Liste(),
-            )));
-  }
-}
-
-class Liste extends StatefulWidget {
-  @override
-  ListeState createState() => ListeState();
-}
-
-class ListeState extends State<Liste> {
-  @override
-  Widget build(BuildContext context) {
-    final liste = {
-      1: "Nudeln",
-      2: "Pesto",
-    };
-    return Text(liste[1]);
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Hinzufügen...',
+              ),
+              autofocus: false,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
